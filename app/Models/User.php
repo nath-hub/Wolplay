843856a45255507b32f2342195dc31767507464d
nath-hub <nathalietaffot@gmail.com>
2026-04-24 09:32:05 +0100

    modification de la logique de connexion pour vérifier l'email et le bannissement avant de générer le token.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, HasUuids;
+    use HasFactory, Notifiable, HasUuids, HasApiTokens;
 
     protected $primaryKey = 'id';
     public $incrementing = false;
