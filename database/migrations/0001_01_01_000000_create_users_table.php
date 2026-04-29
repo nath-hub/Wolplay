4ef7456d6b71c1e2e04ce3f1572764b3e74425f3
nath-hub <nathalietaffot@gmail.com>
2026-04-29 16:24:20 +0100

    suppressions de lastName et ajout de nullable pour firstName et lastName

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->string('firstName')->nullable();
             $table->string('lastName')->nullable();
-            $table->string('lastName');
             $table->timestamp('email_verified_at')->nullable();
 
             $table->string('pending_email')->nullable();
